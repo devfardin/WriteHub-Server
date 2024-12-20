d48d03ec5e67f919ff8ef5e4c3f7c82b95058607
devfardin <contactfardin22@gmail.com>
2024-12-20 18:14:01 +0600

    global error handle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-
+import { StatusCodes } from 'http-status-codes';
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,68 @@
+import { ErrorRequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { TErrorSources } from '../interface/error';
+import { ZodError } from 'zod';
+import hanldeZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
+import handleCatchError from '../errors/handleCatchError';
+import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
+import config from '../config';
+const globalErrorHandle: ErrorRequestHandler = (error, req, res, next) => {
+  let statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = error.message || 'Internal Server Error';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Someting went wrong',
+    },
+  ];
+  if (error instanceof ZodError) {
+    const simplifiedError = hanldeZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error.name === 'CastError') {
+    const simplifiedError = handleCatchError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error.name === 11000 && 'E11000') {
+    const simplifiedError = handleDuplicateError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error instanceof AppError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
+  }
+
+  // return all type of error
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === 'Development' ? error.stack : null,
+  });
+};
+
+export default globalErrorHandle;
